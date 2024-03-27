6662f159863827fcb87b3812fc66351c6b8e986b
Nokokiii <kubanokoki@gmail.com>
2024-03-27 21:57:49 +0100

    bank account abstract class

---
+++ BankAccount.java
@@ -0,0 +1,25 @@
+public abstract class BankAccount {
+    protected String accountNumber;
+    protected double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
