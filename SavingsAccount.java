11298804c127fb21e8863f52f87779259ced4944
Nokokiii <kubanokoki@gmail.com>
2024-03-28 02:02:11 +0100

    i think for now it's good

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,13 +1,37 @@
 public class SavingsAccount extends BankAccount {
     private double interestRate;
 
-    public SavingsAccount(String accountNumber, double balance) {
-        super(accountNumber, balance);
+    public SavingsAccount(double balance, double interestRate) {
+        super(balance);
+        setInterestRate(interestRate);
+    }
+
+    // Interest methods
+    public void addInterest() {
+        this.balance += this.balance * this.interestRate;
+    }
+
+    public void transferInterest(BankAccount account) {
+        validateBankAccount(account);
+        account.deposit(getInterest());
+    }
+
+    // Setters
+    public void setInterestRate(double interestRate) {
+        if (interestRate < 0) {
+            throw new IllegalArgumentException("Interest rate cannot be negative");
+        }
+
         this.interestRate = interestRate;
     }
 
-    public void addInterest() {
-        balance += balance * interestRate;
+    // Getters
+    public double getInterest() {
+        return this.balance * this.interestRate;
+    }
+
+    public double getInterestRate() {
+        return this.interestRate;
     }
 }
 
